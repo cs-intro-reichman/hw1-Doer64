b8be608290727948bda135ba246d9dd66c10a9ca
Dean Orion <60265116+Doer64@users.noreply.github.com>
2024-11-06 18:06:35 +0200

    Completed

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,20 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		double r = Math.random();
+		int a = (int) (r*lim);
+		r = Math.random();
+		int b = (int) (r*lim);
+		r = Math.random();
+		int c = (int) (r*lim);
+
+		System.out.println(a + " " + b + " "+ c);
+
+		int min = Math.min(Math.min(a, b), Math.min(b, c));
+		int max = Math.max(Math.max(a, b), Math.max(b, c));
+		int mid = (a + b + c) - min - max;
+
+		System.out.println(min + " " + mid + " "+ max);
 	}
 }
--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,12 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name1 = args[0];//recives 3 names as String input
+        String name2 = args[1];
+        String name3 = args[2];
+        int fullP = Integer.parseInt(args[3]); //recives full price to pay
+
+        double pay = Math.ceil(fullP/3.0);
+        System.out.println("Dear " + name3 +", "+ name2 + ", and " + name1 + ": pay " + pay + " shekels each");    
 	}
 }
